01ced87bbfa6a18914b0056055ee557095b55393
naluebet mapati <naluebet.m@gmail.com>
2019-06-22 04:24:49 +0700

    commit 26 13/13

--- src/app/authentication/components/profile/profile.component.ts
+++ src/app/authentication/components/profile/profile.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { IProfileComponent } from './profile.interface';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, IProfileComponent {
+  positionItems: any[] = [
+    'Frontend Developer',
+    'Backend Developer'
+  ];
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
+
+
